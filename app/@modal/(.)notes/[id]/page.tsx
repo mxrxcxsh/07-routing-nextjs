eb09a44ec820b2e3e4cef9465a7bf483c4a878f9
Andrii Khomenko <akhomenko1995@gmail.com>
2026-04-07 21:04:56 +0200

    changes due to the review

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,13 +1,29 @@
 import NotePreview from './NotePreview.client';
 import { fetchNoteById } from '@/lib/api';
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from '@tanstack/react-query';
 
 type Props = {
   params: Promise<{ id: string }>;
 };
 
-export default async function NotePage({ params }: Props) {
+const NotePage = async ({ params }: Props) => {
   const { id } = await params;
-  const note = await fetchNoteById(id);
 
-  return <NotePreview note={note} />;
-}
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotePreview />
+    </HydrationBoundary>
+  );
+};
+export default NotePage;
--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,4 +1,10 @@
 import NotesFilterClient from '../NotesFilter.client';
+import { fetchNotes } from '@/lib/api';
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from '@tanstack/react-query';
 
 interface NotesFiltersProps {
   params: Promise<{ slug: string[] }>;
@@ -8,9 +14,21 @@ async function NotesFilters({ params }: NotesFiltersProps) {
   const { slug } = await params;
   const tag = slug[0] === 'all' ? undefined : slug[0];
 
+  const queryClient = new QueryClient();
+  const page = 1;
+  const perPage = 12;
+  const query = '';
+
+  await queryClient.prefetchQuery({
+    queryKey: ['notes', 'filter', tag ?? 'all', page, query],
+    queryFn: () => fetchNotes({ page, perPage, tag, query }),
+  });
+
   return (
     <div>
-      <NotesFilterClient tag={tag} />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <NotesFilterClient tag={tag} />
+      </HydrationBoundary>
     </div>
   );
 }
